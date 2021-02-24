0b5bb5c464a8eb2ea9efe486f4a9a8899393ec5d
mirsaperez <77262010+mirsaperez@users.noreply.github.com>
2021-02-24 11:15:05 -0800

    replaced all of the code with 3.1.6 code given

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,16 +1,16 @@
-// this creates a prompt named "What is your robots name"
+var playerName = window.prompt("What is your robot's name?");
+var playerHealth = 100;
+var playerAttack = 10;
 
-var playerName =  window.prompt("What is your robots name");
-// Note the lack of quotation marks around  playerName
-window.alert(playerName);
+// You can also log multiple values at once like this
+console.log(playerName, playerAttack, playerHealth);
 
+var enemyName = "Roborto";
+var enemyHealth = 50;
+var enemyAttack = 12;
 
+var fight = function() {
+  window.alert("Welcome to Robot Gladiators!");
+};
 
-
-
-// this creates a function named "fight"
-
-function fight() {
-    window.alert("The fight has begun!");
-  }
-  // fight();
+fight();
